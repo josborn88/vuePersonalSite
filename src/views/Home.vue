d6f649bc648defeae3b1476a7befaee017b1d277
John Osborn <josborn88@gmail.com>
2020-01-04 09:11:43 -0500

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="main">
+    <div class="sidemenu">
+      <SideBar></SideBar>
+    </div>
+    
+    <div class="body">
+      <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import SideBar from '@/components/SideBar.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld,
+    SideBar
+  }
+}
+</script>
+
+<style>
+  .main{
+    display: grid;
+     grid-template-columns: 1fr 2fr 2fr;
+    grid-template-areas: 
+        "sidemenu   body";
+  }
+
+  .sidemenu {
+    background-color: red;
+  }
+  .body {
+    background-color: blue;
+    text-align: center;
+
+  }
+
+</style>
